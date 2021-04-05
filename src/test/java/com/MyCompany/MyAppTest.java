7f2dfbc5ceec5947fa1c9c6f03ab4272abd2235e
deepa <dvdkadakol@gmail.com>
2021-04-05 11:59:23 +0530

    First Commit to repo

---
+++ src/test/java/com/MyCompany/MyAppTest.java
@@ -0,0 +1,51 @@
+package com.MyCompany;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.myCompany.MyApp;
+
+import org.junit.After;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class MyAppTest
+{
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void testAppConstructor() {
+        try {
+            new MyAppTest();
+        } catch (Exception e) {
+            fail("Construction failed.");
+        }
+    }
+
+    @Test
+    public void testAppMain()
+    {
+        MyApp.main(null);
+        try {
+            assertEquals("Hello World" + System.getProperty("line.separator"), outContent.toString());
+        } catch (AssertionError e) {
+            fail("\"message\" is not \"Hello World\"");
+        }
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+}
